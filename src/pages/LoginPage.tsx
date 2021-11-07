eb1ea579c1a976641c66b5b26b055b3a99a58eaf
vazsonyidl <danielvazsonyi@gmail.com>
2021-11-07 16:43:58 +0100

    feat(login): navigate after successful login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import {TextField, Box, Button} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
 
 import {loginWithEmail} from '../firebase-api/auth';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const onUserNameChange = ({target: {value}}: React.ChangeEvent<HTMLInputElement>): void => {
     setEmail(value);
@@ -16,7 +18,7 @@ const LoginPage = () => {
   };
 
   const onLoginClick = (): void => {
-    loginWithEmail(email, password);
+    loginWithEmail(email, password).then((cred) => void navigate('/about'));
   };
 
   return (
