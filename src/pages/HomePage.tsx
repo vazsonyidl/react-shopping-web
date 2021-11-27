cfec7c562d5372021d2d7f8b78af7c5a037d6e35
vazsonyidl <danielvazsonyi@gmail.com>
2021-11-27 17:45:12 +0100

    feat(home-page): add dropdown select to select which week to check out
    Prefetch the week options and display it in a select list.
    On selection change, fetch and display the elements corresponding to that week.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,57 @@
 import React, {useEffect, useState} from 'react';
-import {fetchItems} from '../firebase-api/rest';
+import {FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from '@mui/material';
+
+import {fetchDocuments, fetchItems} from '../firebase-api/rest';
 import {ShoppingListItemType} from '../models/rest.interface';
 import ListItem from '../components/ListItem/ListItem';
 
 const collectionPath = 'elements';
-const documentPath = 'week_one';
 
 const HomePage = () => {
-  const [listItems, setListItems] = useState<Array<ShoppingListItemType & {name: string}>>([]);
+  const [listItems, setListItems] = useState<Array<ShoppingListItemType & { name: string }>>([]);
+  const [documentId, setDocumentId] = useState<string>('');
+  const [documentIds, setDocumentIds] = useState<Array<string>>([]);
 
   useEffect(() => {
-    fetchItems(collectionPath, documentPath)
-      .then(snapshot => snapshot.data() || {})
-      .then((data: {[key: string]: ShoppingListItemType}) => {
-        setListItems(Object.keys(data || {}).map((key) => ({...data[key], name: key})));
-      });
+    fetchDocuments(collectionPath)
+      .then(documents => documents.docs.map(doc => doc.id))
+      .then((documentIds: Array<string>) => setDocumentIds(documentIds));
   }, []);
+
+  const handleWeekSelectChange = (event: SelectChangeEvent) => {
+    event.preventDefault();
+    setDocumentId(event?.target?.value);
+    fetchElementsByDocumentId(event?.target?.value)
+      .then((items: Array<ShoppingListItemType & {name: string}>) => setListItems(items));
+  };
+
+  function fetchElementsByDocumentId(documentId: string): Promise<Array<ShoppingListItemType & {name: string}>> {
+    return fetchItems(documentId)
+      .then(snapshot => snapshot.data() || {})
+      .then((data: { [key: string]: ShoppingListItemType }) => Object.keys(data || {}).map((key) => ({
+        ...data[key],
+        name: key
+      })));
+  }
+
   return (
     <>
+      <FormControl fullWidth>
+        <InputLabel id="week-select-input-label">Week</InputLabel>
+        <Select
+          labelId="week-select-input-label"
+          id="week-select-input"
+          label="Week"
+          value={documentId}
+          onChange={handleWeekSelectChange}
+        >
+          {documentIds.map(id => <MenuItem value={id} key={id}>{id}</MenuItem>)}
+        </Select>
+      </FormControl>
+
       {listItems.map((item, index) => <ListItem item={item} key={index}/>)}
     </>
-  )
-}
+  );
+};
 
 export default HomePage;
